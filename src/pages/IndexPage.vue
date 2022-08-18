6cf0167189c4e5d711673fd6ee23984d1a9714b0
vjulia <julia000jv@gmail.com>
2022-08-18 18:30:48 +0100

    primeiro commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="main flex flex-center">
+  <q-card class="flex form-login">
+    <div class="img-logo">
+     <img
+        alt="imagem-google"
+        src="~assets/logo.png"
+       style="width: 90px; height: 34px" >
+    </div>
+  </q-card>
+</div>
+</template>
+<style>
+.main {
+  background: #fff;
+  height: 100vh;
+  width: 100vw;
+}
+
+.form-login {
+  height: 500px;
+  width: 350px;
+  margin: auto;
+}
+
+.img-logo {
+  margin-top: 30px;
+}
+/*
+.imagem-login-gmail {
+  margin: auto 550px;
+}
+
+.email-telefone {
+  margin-bottom: auto 300px;
+}
+
+.item { width: 500px;
+  height: 500px;
+  border: 1px solid black;
+  margin-left: 500px;
+  margin-top: 10px;
+}
+*/
+</style>
